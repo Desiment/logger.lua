230a59862cb3e1659a618b6a604d9353820918b2
Desiment <desiment@yandex.ru>
2023-02-19 03:06:41 +0300

    Small changes in code

--- logger.lua
+++ logger.lua
@@ -2,84 +2,110 @@
 -- logger.lua
 --
 
-local log = { _version = "0.1.0" }
+local logger = { _version = "0.1.0" }
 
-log.usecolor = true
-log.outfile = nil
-log.level = "trace"
-
-
-local modes = {
-  { name = "trace", color = "\27[34m", },
-  { name = "debug", color = "\27[36m", },
-  { name = "info",  color = "\27[32m", },
-  { name = "warn",  color = "\27[33m", },
-  { name = "error", color = "\27[31m", },
-  { name = "fatal", color = "\27[35m", },
+logger.modes = {
+  { name = "trace",   color = "\27[34m", depth = 4},
+  { name = "debug",   color = "\27[36m", depth = 3},
+  { name = "info",    color = "\27[32m", depth = 2},
+  { name = "success", color = "\27[35m", depth = 2},
+  { name = "warning", color = "\27[33m", depth = 2},
+  { name = "error",   color = "\27[31m", depth = 1},
+  { name = "fatal",   color = "\27[35m", depth = 0},
 }
 
+logger.depth  = 4
+logger.output = "console" -- other option is "file"
+logger.file   = "log"     -- in case of writing into a file
 
-local levels = {}
-for i, v in ipairs(modes) do
-  levels[v.name] = i
+-- helping functions ---
+
+local _obj_to_string
+local _tbl_to_string
+local _arg_to_string
+local _msg_to_string
+local _get_info
+
+_obj_to_string = function(x)
+	if type(x) == 'table' then
+		return _tbl_to_string(x)
+	else
+		return tostring(x)
+	end
+end
+
+_tbl_to_string = function(tbl)
+	result = ''
+	for k, v in pairs(tbl) do
+		result = result .. ', ' .. '[' .. _obj_to_string(k) .. '] = ' ..  _obj_to_string(v)
+	end
+	result = '{' .. result:sub(3) .. '}'
+	return result
+end
+
+_arg_to_string = function(...)
+	args = {...}
+	return _obj_to_string({...})
+end
+
+_msg_to_string = function(...)
+	args   = {...}
+	result = ''
+	for _, x in pairs(args) do
+		result = result .. ' ' ..  _obj_to_string(x)
+	end
+	return result
+end
+
+_get_info = function(dlevel)
+	local dlevel = dlevel or 3
+	return debug.getinfo(dlevel, "Sl").short_src .. ":" .. debug.getinfo(dlevel, "Sl").currentline
 end
 
 
-local round = function(x, increment)
-  increment = increment or 1
-  x = x / increment
-  return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * increment
+logger._args = _arg_to_string
+logger._msg  = _msg_to_string
+logger._info = _get_info
+
+
+-- logger writers --
+
+logger._write_console = function(name, message, info, color)
+	print(string.format("%s[%-8s%s]%s %s: %s", color, name, os.date("%H:%M:%S"), "\27[0m", info, message))
+end
+
+logger._write_file = function(name, message, info, color, filename)
+	fp = io.open(logger.file, "a")
+	fp:write(string.format("[%-8s%s] %s: %s\n", name, os.date(), info, message))
+	fp:close()
+end
+
+logger._write = function(...)
+	if logger.output == "console" then
+		logger._write_console(...)
+	elseif logger.output == "file" then
+		logger._write_file(..., logger.file)
+	end
 end
 
 
-local _tostring = tostring
-
-local tostring = function(...)
-  local t = {}
-  for i = 1, select('#', ...) do
-    local x = select(i, ...)
-    if type(x) == "number" then
-      x = round(x, .01)
-    end
-    t[#t + 1] = _tostring(x)
-  end
-  return table.concat(t, " ")
+for _, md in ipairs(logger.modes) do
+	logger[md.name] = function(...)
+		if md.depth <= logger.depth then
+			logger._write(md.name:upper(), logger._msg(...), logger._info(), md.color)
+		end
+	end
 end
 
-
-for i, x in ipairs(modes) do
-  local nameupper = x.name:upper()
-  log[x.name] = function(...)
-    
-    -- Return early if we're below the log level
-    if i < levels[log.level] then
-      return
-    end
-
-    local msg = tostring(...)
-    local info = debug.getinfo(2, "Sl")
-    local lineinfo = info.short_src .. ":" .. info.currentline
-
-    -- Output to console
-    print(string.format("%s[%-6s%s]%s %s: %s",
-                        log.usecolor and x.color or "",
-                        nameupper,
-                        os.date("%H:%M:%S"),
-                        log.usecolor and "\27[0m" or "",
-                        lineinfo,
-                        msg))
-
-    -- Output to log file
-    if log.outfile then
-      local fp = io.open(log.outfile, "a")
-      local str = string.format("[%-6s%s] %s: %s\n",
-                                nameupper, os.date(), lineinfo, msg)
-      fp:write(str)
-      fp:close()
-    end
-
-  end
+-- log-on-call wrapper --
+logger.call = {}
+for _, md in ipairs(logger.modes) do
+	logger.call[md.name] = function(func)
+		return (function(...)
+			logger._write(md.name:upper(), logger._msg("Registered function call; args = ", logger._args(...)), logger._info(), md.color)
+			return func(...)
+		end)
+	end
 end
 
-
-return log
+return logger
